7abe908edd421132807372a05e7bab6da546b1a6
Liav Levi <liavmoshe12345@gmail.com>
2023-04-10 22:35:56 +0300

    removed double print

--- sources/game.cpp
+++ sources/game.cpp
@@ -104,7 +104,6 @@ Game::Game(Player& p1, Player& p2): p1(p1),p2(p2){
     
     void Game::printLastTurn(){
         cout << this->logs.back();
-        cout << this->logs.back(); 
     };
 
     void Game::playAll(){
